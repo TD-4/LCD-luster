426ac8418d2e65da65af17797d68328ae243223c
unknown <f04396@lusterinc.com>
2021-08-25 14:05:17 +0800

    模型

--- models/EfficientNet.py
+++ models/EfficientNet.py
@@ -1,3 +1,8 @@
+# _*_coding:utf-8_*_
+# @auther:FelixFu
+# @Data:2021.4.16
+# @github:https://github.com/felixfu520
+
 from efficientnet_pytorch import EfficientNet
 from torch import nn
 import torch
@@ -7,6 +12,7 @@ class EfficientNet_b0(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b0, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b0', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -17,6 +23,7 @@ class EfficientNet_b1(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b1, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b1', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -27,6 +34,7 @@ class EfficientNet_b2(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b2, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b2', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -37,6 +45,7 @@ class EfficientNet_b3(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b3, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b3', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -47,6 +56,7 @@ class EfficientNet_b4(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b4, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b4', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -57,6 +67,7 @@ class EfficientNet_b5(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b5, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b5', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -67,6 +78,7 @@ class EfficientNet_b6(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b6, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b6', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -77,6 +89,7 @@ class EfficientNet_b7(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b7, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b7', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
@@ -87,6 +100,7 @@ class EfficientNet_b8(torch.nn.Module):
     def __init__(self, num_class=1000, in_channels=3, pretrained=False, freeze_bn=False):
         super(EfficientNet_b8, self).__init__()
         self.model = EfficientNet.from_pretrained(model_name='efficientnet-b8', in_channels=in_channels)
+        self.model.set_swish(memory_efficient=False)
         self.model._fc = nn.Linear(in_features=self.model._fc.in_features, out_features=num_class, bias=True)
 
     def forward(self, x):
